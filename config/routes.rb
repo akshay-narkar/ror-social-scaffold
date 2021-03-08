df80e133099cc94884fa099000c9f5a073f4f42f
Gilbert Gotora <gilbertgotora2@gmail.com>
2021-03-08 14:34:26 +0200

    remove comments routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show] 
     resources :friendships
-    # , only: [:create,:destory,:index,:show] 
   resources :posts, only: [:index, :create] do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
   end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
