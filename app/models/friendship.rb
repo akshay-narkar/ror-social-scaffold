8d6209e835a8b8cb108d8226e83b48b20cdb5785
akshay-narkar <akshaynarkar1295@gmail.com>
2021-03-13 22:06:44 +0530

    Add after_update callback in friendships model.So 2 rows are created for a friendship to be completed rather than 1.

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -4,8 +4,10 @@ class Friendship < ApplicationRecord
   validates_uniqueness_of :user_id, scope: %i[friend_id]
   validates_uniqueness_of :friend_id, scope: %i[user_id]
 
-  scope :friendlist, -> { where('status = ?', true) }
+  # scope :friendlist, -> { where('status = ?', true) }
   scope :waiting, -> { where('status = ?', false) }
+
+  after_update :create_friend
 
   def await(current_user, user)
     x = Friendship.where('friend_id = ?', current_user.id).and(Friendship.where('user_id = ?', user.id)).waiting
@@ -16,18 +18,26 @@ class Friendship < ApplicationRecord
     x2.empty? ? nil : x2
   end
 
-  def friendss(current_user, user)
-    y = Friendship.where('friend_id = ?', current_user.id).and(Friendship.where('user_id = ?',
-                                                                                user.id)).friendlist
+  def friendss(current_user, _user)
+    # y = Friendship.where('friend_id = ?', current_user.id).and(Friendship.where('user_id = ?',
+    #                                                                             user.id)).friendlist
 
-    y1 = Friendship.where('friend_id = ?', user.id).and(Friendship.where('user_id = ?', current_user.id)).friendlist
+    # y1 = Friendship.where('friend_id = ?', user.id).and(Friendship.where('user_id = ?', current_user.id)).friendlist
 
-    y2 = y + y1
+    # y2 = y + y1
+
+    y2 = current_user.friendships
 
     y2.empty? ? nil : y2
   end
 
   def pendingreq(currentuser)
     Friendship.where('friend_id = ?', currentuser.id).waiting
+  end
+
+  private
+
+  def create_friend
+    Friendship.create(status: true, friend_id: user_id, user_id: friend_id)
   end
 end
